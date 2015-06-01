74844f365ec01c578ec5ed865c7c406e9c63d090
Patryk Kalinowski <patryk.kalinowski@gmail.com>
2015-06-02 00:05:07 +0200

    Set permits to edit fields

--- app/admin/language.rb
+++ app/admin/language.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Language do
-
+  permit_params :name, :enabled
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
